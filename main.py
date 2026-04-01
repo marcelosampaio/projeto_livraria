946054470bd8d46690274783db449d5dba07cbb6
Marcelo Sampaio <marcelosampaiio1965@gmail.com>
2026-04-01 13:43:32 -0300

    feat: html parser

--- main.py
+++ main.py
@@ -9,7 +9,13 @@ resposta = requests.get(URL)
 
 # response status code
 if resposta.status_code != 200:
-    print(f"Erro ao acessar o site. Status code: {resposta.status_code}")
+    print(f"Erro no http request. Status code: {resposta.status_code}")
     exit()
-else:
-    print("Request successful!")
+    
+print("Conexão bem-sucedida!")
+
+# beatifulsoap object
+soup = BeautifulSoup(resposta.text, "html.parser")
+
+# html parsing
+print(soup.title.string.strip())
